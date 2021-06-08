54486b87db5ffb53c3dff56cbed5d3cc63d13fb8
ebetron <ebetron@gmail.com>
2021-06-08 18:37:36 -0400

    About Page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+// markup
+const AboutPage = () => {
+  return (
+    <main>
+     <h1>About</h1>
+     <Link to="/">Back to Home</Link>
+    </main>
+  )
+}
+
+export default AboutPage
