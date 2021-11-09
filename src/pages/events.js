8a8cc7e6b0d182a88f6d7af1df694f1e2dc359ec
ebetron <ebetron@gmail.com>
2021-11-09 18:03:05 -0500

    Events - H1 Fix

--- src/pages/events.js
+++ src/pages/events.js
@@ -7,7 +7,7 @@ const Events = () => {
         <div className="h-screen pb-14 bg-right bg-cover" style={{ backgroundImamage: `url('bg.svg')` }}>
             <Navbar />
             <div className="container pt-12 md:pt-24 px-6 mx-auto flex flex-wrap flex-col md:flex-row">
-                <h1 className="my-4 text-3xl md:text-5xl text-blue-400 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Events</h1>
+                <h1 className="my-4 text-3xl md:text-5xl text-blue-400 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Eventos</h1>
                 <div>
                     <p className="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">Nuestros Hack Nights (Noches de Colaboración) se llevan a cabo los primeros martes de cada mes. Para estar al tanto de esta y otras actividades de la brigada, unanse a nuestro grupo en la plataforma Meetup</p>
                 </div>
