fa3ee15a332ca428717af9d72d441643e35dac6f
ebetron <ebetron@gmail.com>
2022-01-31 20:55:54 -0500

    Contact Page using Bootstrap

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,18 +6,18 @@ const Contact = () => {
     return (
         <div>
             <Navbar />
-            <div className="container pt-12 md:pt-24 px-6 mx-auto flex flex-wrap flex-col md:flex-row">
-                <h1 className="my-4 text-3xl md:text-5xl text-blue-400 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Contacto</h1>
+            <div className="container mt-5">
+                <h1 className="my-4 text-primary">Contacto</h1>
                 <div>
-                    <h2 className="my-2 text-blue-400 text-xl font-bold slide-in-bottom-h2">Slack</h2>
+                    <h2 className="my-2 text-secondary">Slack</h2>
                     <p>Crea una cuenta y accede a los diferentes canales de nuestros proyectos. La plataforma Slack es el principal espacio para que los equipos y proyectos coordinen sus esfuerzos. La mayor parte de la comunicación pasara aquí fuera de nuestros hack nights.</p>
-                    <div className="flex w-full justify-center md:justify-start pb-8 lg:pb-0 fade-in mt-2">
-					    <a href="https://chat.code4puertorico.org/" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">ACCESA SLACK DE CODE4PR</a>
+                    <div>
+					    <a href="https://chat.code4puertorico.org/" className="btn btn-primary">ACCESA SLACK DE CODE4PR</a>
 					</div>
                 </div>
                 <div className="mt-2">
-                    <h2 className="my-2 text-blue-400 text-xl font-bold slide-in-bottom-h2">Redes Sociales</h2>
-                    <ul className="list-disc list-inside">
+                    <h2 className="my-3 text-secondary">Redes Sociales</h2>
+                    <ul>
                         <li><a className="text-blue-500" href="https://www.meetup.com/code-for-puerto-rico/">Meetup</a></li>
                         <li><a className="text-blue-500" href="https://twitter.com/code4puertorico">Twitter</a></li>
                         <li><a className="text-blue-500" href="https://www.linkedin.com/company/code-for-puerto-rico">LinkedIn</a></li>
